964ddf0a32ae4a54efb6b2d6a093d9c9ecb85cdd
Peter Koellner <peter@asgalon.net>
2025-12-05 08:36:58 -0500

    3 output format

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,6 @@
 ''' emotion_detection module - provide emotion_detector method '''
+import json
+import logging
 import requests
 
 # python does not have real constants, this is just a naming convention...
@@ -7,8 +9,15 @@ EMD_SERVER = 'https://sn-watson-emotion.labs.skills.network'
 EMD_PATH = '/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
 EMD_URL = EMD_SERVER + EMD_PATH
 EMD_HEADERS = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-EMD_BLANK_RESPONSE = '{ "error": "Please enter text" }'
-EMD_ERROR_RESPONSE = '{ "error": "Backend can\'t process input" }'
+EMD_BLANK_RESPONSE = { "error": "Please enter text" }
+EMD_ERROR_RESPONSE = { "error": "Backend can't process input" }
+
+EMD_EMOTION_PREDICTIONS = 'emotionPredictions'
+EMD_EMOTION = 'emotion'
+
+DOMINANT = 'dominant_emotion'
+
+LOG = logging.getLogger(__name__)
 
 def emotion_detector(text_to_analyze):
     ''' Call backend for emotion prediction '''
@@ -21,4 +30,81 @@ def emotion_detector(text_to_analyze):
     if response.status_code != 200:
         return EMD_ERROR_RESPONSE
 
-    return response.text
+    response_json = json.loads(response.text)
+
+    LOG.debug(response_json)
+
+    if EMD_EMOTION_PREDICTIONS not in response_json:
+        return EMD_ERROR_RESPONSE
+
+    #
+    # Sample output:
+    #
+    # { 'emotionPredictions': [
+    #    {
+    #        'emotion': {
+    #            'anger': 0.0043079085,
+    #            'disgust': 0.00041127237,
+    #            'fear': 0.0037504788,
+    #            'joy': 0.9918804,
+    #            'sadness': 0.014091322},
+    #        'target': '',
+    #        'emotionMentions': [
+    #            {
+    #                'span': {
+    #                    'begin': 0,
+    #                    'end': 10,
+    #                    'text': 'I am happy'
+    #                },
+    #                'emotion': {
+    #                    'anger': 0.0043079085,
+    #                    'disgust': 0.00041127237,
+    #                    'fear': 0.0037504788,
+    #                    'joy': 0.9918804,
+    #                    'sadness': 0.014091322
+    #                }
+    #            }
+    #        ]
+    #    }
+    #   ],
+    #   'producerId': {
+    #       'name': 'Ensemble Aggregated Emotion Workflow',
+    #       'version': '0.0.1'
+    #   }
+    # }
+    #
+    # for this service, all that is of interest are the entries in
+    # result['emotionPredictions'][0]['emotion']
+    #
+
+    emotion_predictions = response_json[EMD_EMOTION_PREDICTIONS]
+
+    LOG.debug(emotion_predictions)
+
+    if not isinstance(emotion_predictions,list or len(emotion_predictions) == 0):
+        return EMD_ERROR_RESPONSE
+
+    LOG.debug(emotion_predictions[0])
+
+    if EMD_EMOTION not in emotion_predictions[0]:
+        return EMD_ERROR_RESPONSE
+
+    emotions = response_json[EMD_EMOTION_PREDICTIONS][0][EMD_EMOTION]
+
+    LOG.debug(emotions)
+
+    # after checking for a sane structure, we can now extract the 'emotion' entries
+    # adding the dominant emotion
+
+    dominant = None
+    highest_score = 0.0
+    return_values = {}
+
+    for name,score in emotions.items():
+        return_values[name] = score
+        if score > highest_score:
+            highest_score = score
+            dominant = name
+
+    return_values[DOMINANT] = dominant
+    return return_values
--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -1,6 +1,5 @@
 
 ''' Testing emotion_detection module '''
-import json
 import unittest
 from emotion_detection import emotion_detector
 
@@ -8,19 +7,18 @@ class TestEmotionDetection(unittest.TestCase):
     ''' emotion detection test cases '''
     def test_emotion_detector_regular(self):
         '''Connect to backend to see if request is set up correctly.'''
-        text = emotion_detector("I am happy")
-        self.assertNotEqual(text,None)
-        self.assertTrue(isinstance(text,str))
-        response_json = json.loads(text)
+        response_json = emotion_detector("I am happy")
+        self.assertNotEqual(response_json, None)
+        self.assertTrue(isinstance(response_json, dict))
 
-        self.assertTrue('emotionPredictions' in response_json)
+        self.assertTrue('dominant_emotion' in response_json)
+        self.assertEqual(response_json['dominant_emotion'],'joy')
 
     def test_emotion_detector_empty(self):
         ''' Empty text parameter should return error entry.'''
-        text = emotion_detector("")
-        self.assertNotEqual(text,None)
-        self.assertTrue(isinstance(text,str))
-        response_json = json.loads(text)
+        response_json = emotion_detector("")
+        self.assertNotEqual(response_json, None)
+        self.assertTrue(isinstance(response_json, dict))
 
         self.assertTrue('error' in response_json)
         self.assertEqual(response_json['error'], "Please enter text")
